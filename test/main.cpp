40fd37a5cd98afe7ffb114570bdca3488e9b7b47
irfan <1964547+kerneltraceroute@users.noreply.github.com>
2018-12-06 16:23:34 +0300

    Runner
    Template Test Function

---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "../src/APM.h"
+
+int main() {
+
+	std::cout << "Application Performance Management Log Parser and Reporter is start!..\n";
+	
+	
+	
+	
+
+	return 0;
+}
